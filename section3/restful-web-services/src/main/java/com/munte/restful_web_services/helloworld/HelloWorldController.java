85cb3835c7d90f2d8aa5296b7d8d8aeed927d3ba
BogdanSicu <b0gdan.munt3@gmail.com>
2026-07-14 16:10:34 +0300

    Added internationalization messages

--- section3/restful-web-services/src/main/java/com/munte/restful_web_services/helloworld/HelloWorldController.java
+++ section3/restful-web-services/src/main/java/com/munte/restful_web_services/helloworld/HelloWorldController.java
@@ -1,13 +1,25 @@
 package com.munte.restful_web_services.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping()
     public String helloWorld() {
@@ -22,6 +34,14 @@ public class HelloWorldController {
     @GetMapping("/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable(name = "name") String pathVariable) {
         return new HelloWorldBean("Hello " + pathVariable + "!");
+    }
+
+    @GetMapping("/internationalized")
+    public String goodMorningInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale(); // get the location of the user from the headers
+
+        return messageSource.getMessage("good.morning.message", null, "default message", locale);
+
     }
 
 }
